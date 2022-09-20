72d9347957c4c17722719edf6a0011fc6afacf67
Squad 5 <nago.thegloriouswalrus@gmail.com>
2022-09-19 22:17:15 -0500

    Minor garage tweaks (more to do)

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 AutoRespawn = false --True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
-SharedGarages = false   --True == Gang and job garages are shared, false == Gang and Job garages are personal
+SharedGarages = true   --True == Gang and job garages are shared, false == Gang and Job garages are personal
 VisuallyDamageCars = true --True == Visually damage cars that go out of the garage depending of body damage, false == Do not visually damage cars (damage is still applied to car values)
 
 Garages = {
@@ -27,9 +27,9 @@ Garages = {
     },
     ["chumashplaza"] = {
         label = "Chumash Plaza Parking",
-        takeVehicle = vector3(-3162.25, 1085.03, 20.85),
-        spawnPoint = vector4(-3158.17, 1077.16, 20.69, 247.66),
-        putVehicle = vector3(-3155.83, 1084.99, 20.71),
+        takeVehicle = vector3(-3158.18, 1126.55, 20.84),
+        spawnPoint = vector4(-3174.18, 1107.37, 20.83, 335.07),
+        putVehicle = vector3(-3174.18, 1107.37, 20.83),
         showBlip = true,
         blipName = "Public Parking",
         blipNumber = 357,
@@ -228,7 +228,7 @@ Garages = {
         type = 'public',                --public, job, gang, depot
         vehicle = 'sea'                 --car, air, sea
     },
-    ["paleto_sea_bh"] = {
+    ["boat_paleto"] = {
         label = "Paleto Pier",
         takeVehicle = vector3(-277.46, 6637.2, 7.48),
         spawnPoint = vector4(-289.2, 6637.96, 1.01, 45.5),
@@ -239,46 +239,46 @@ Garages = {
         type = 'public',                --public, job, gang, depot
         vehicle = 'sea'                 --car, air, sea
     },
-    ["catfish_sea_bh"] = {
+    ["boat_catfish"] = {
         label = "Catfish Pier",
-        takeVehicle = vector3(3856.6, 4459.71, 1.84),
-        spawnPoint = vector4(3856.36, 4453.86, 0.29, 270.41),
-        putVehicle = vector3(3856.36, 4453.86, 0.29),
+        takeVehicle = vector3(3813.39, 4448.76, 3.4),
+        spawnPoint = vector4(3860.93, 4447.22, 0.42, 274.85),
+        putVehicle = vector3(3860.93, 4447.22, 0.42),
         showBlip = true,
         blipName = "Boat Launch",
         blipNumber = 356,
         type = 'public',                --public, job, gang, depot
         vehicle = 'sea'                 --car, air, sea
     },
-    ["millarsSeaBH"] = {
-        label = "Millars Boathouse",
-        takeVehicle = vector3(1299.24, 4216.69, 33.9),
-        spawnPoint = vector4(1297.82, 4209.61, 30.12, 253.5),
-        putVehicle = vector3(1297.82, 4209.61, 30.12),
+    ["boat_millars"] = {
+        label = "Millars Marina",
+        takeVehicle = vector3(1382.47, 4306.77, 36.79),
+        spawnPoint = vector4(1421.31, 4247.81, 30.34, 213.94),
+        putVehicle = vector3(1421.31, 4247.81, 30.34),
         showBlip = true,
         blipName = "Boat Launch",
         blipNumber = 356,
         type = 'public',                --public, job, gang, depot
         vehicle = 'sea'                 --car, air, sea
     },
-    /*["sandySeaDepot"] = {
+    ["boat_sandy"] = {
         label = "Sandy Marina",
         takeVehicle = vector3(1447.28, 3761.85, 31.73),
         spawnPoint = vector4(1443.38, 3766.82, 30.19, 298.22),
         putVehicle = vector3(1443.38, 3766.82, 30.19),
         showBlip = true,
-        blipName = "Boat Depot [Impound]",
+        blipName = "Boat Launch",
         blipNumber = 356,
         type = 'depot',                --public, job, gang, depot
         vehicle = 'sea'                 --car, air, sea
-    },*/
-    ["paletoSeaDepot"] = {
+    },
+    ["boat_depot_paleto"] = {
         label = "Paleto Marina",
         takeVehicle = vector3(-278.3, 6621.67, 7.5),
         spawnPoint = vector4(-285.05, 6620.64, 0.83, 46.28),
         putVehicle = vector3(-285.05, 6620.64, 0),
         showBlip = true,
-        blipName = "Boat Depot [Impound]",
+        blipName = "Paleto Marina [Depot]",
         blipNumber = 356,
         type = 'depot',                --public, job, gang, depot
         vehicle = 'sea'                 --car, air, sea
