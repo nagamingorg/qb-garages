ac5b0efffcb14945a1658e393a5c114fecce38ad
Squad 5 <nago.thegloriouswalrus@gmail.com>
2023-04-02 12:27:51 -0500

    Actually fix linting
    if this doesnt work idk anymore

--- config.lua
+++ config.lua
@@ -1,21 +1,9 @@
-Config = Config or {}
+Config = {}
 Config.AutoRespawn = false --True == auto respawn cars that are outside into your garage on script restart, false == does not put them into your garage and players have to go to the impound
 Config.SharedGarages = false   --True == Gang and job garages are shared, false == Gang and Job garages are personal
 Config.VisuallyDamageCars = true --True == Visually damage cars that go out of the garage depending of body damage, false == Do not visually damage cars (damage is still applied to car values)
 
 Config.Garages = {
-    /*["casinogarage"] = {
-        ["label"] = "Casino Parking",
-        ["takeVehicle"] = vector3(925.3, 51.85, 81.11),
-        ["spawnPoint"] = vector4(918.77, 51.73, 80.16, 329.43),
-        ["putVehicle"] = vector3(914.92, 37.85, 80.07),
-        ["showBlip"] = true,
-        ["blipName"] = "Public Parking",
-        ["blipNumber"] = 357,
-        ["blipColor"] = 39,
-        ["type"] = "public",                --public, job, gang, depot
-        ["vehicle"] = "car"                 --car, air, sea, rig
-    },*/
     ["themotorhotel"] = {
         ["label"] = "The Motor Hotel Parking",
         ["takeVehicle"] = vector3(1137.77, 2663.54, 37.9),
